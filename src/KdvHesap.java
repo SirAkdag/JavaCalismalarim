0b68e3a497bc9751b51c82c49f873d711395e3f0
Emre Akdag <emreakdag@windowslive.com>
2022-01-19 21:43:40 +0100

    kdv hesaplama

--- src/KdvHesap.java
+++ src/KdvHesap.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class KdvHesap {
+import java.util.Scanner;
+
+public class KdvHesap {
+    public static void main(String[] args) {
+        Scanner inp= new Scanner(System.in);
+        double kdvOran= 0.18,kdvTutari, kdvliTutar;
+        System.out.print("Lütfen bir tutar giriniz : ");
+        int fiyat= inp.nextInt();
+        kdvTutari= fiyat*kdvOran;
+        kdvliTutar = kdvTutari+ fiyat;
+        System.out.println("KDV tutari : " + kdvTutari);
+        System.out.print("KDV dahil tutar : "+ kdvliTutar);
+
+
+    }
 }
