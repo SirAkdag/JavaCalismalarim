46dabdd44a61b7e17c96329b8fca39572c0b365d
Emre Akdag <emreakdag@windowslive.com>
2022-01-19 17:54:26 +0100

    son degisiklikler kaydedildi

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 7;
-        int b = 5;
-        b += a;
+     /**   int a=3, b=4, c=5;
+        boolean durum1= a==b;
+        boolean durum2= b<c;
+        boolean sonuc= durum1||durum2;
+        String str = sonuc? "Dogru" : "Degil";
+        System.out.print(str);
+      */
 
-        System.out.println(b);
+        /**char b = 124 ;
+        System.out.print(b);
+        */
+
+        /**int a=3,b=4,c=2;
+        int sonuc= a+b;
+        System.out.print("islemin Sonucu " + sonuc);
+         */
+
+        /**float a= 1.5f;
+        float b= 2.5f;
+        float sonuc= a*b;
+        System.out.print("islem Sonucu = " + sonuc);
+         */
+        /**double a= 1.5, b=2.5;
+        double sonuc= a/b;
+        System.out.print("Islemin Sonucu=" + sonuc);
+         */
+
     }
 }
+
 
