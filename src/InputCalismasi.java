83291ca65713d69d7cacdc67fa5a48e62ad6fd79
Emre Akdag <emreakdag@windowslive.com>
2022-01-19 18:43:19 +0100

    yeni ders eklendi

--- src/InputCalismasi.java
+++ src/InputCalismasi.java
@@ -1,5 +1,12 @@
-public class Main2 {
+import java.util.Scanner;
+
+public class InputCalismasi {
     public static void main(String[] args) {
-        System.out.println("helooo");
+        Scanner herHangiBirIsim= new Scanner(System.in);
+        System.out.print("Adinizi kodlayiniz : ");
+        String str= herHangiBirIsim.nextLine();
+        System.out.print("Tesekkürler " + str + " Bey/Hanim");
+
+
     }
 }
