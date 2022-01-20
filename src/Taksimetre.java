522e7a45f9a8a6b4a46d5afb1bab23cd60c527bd
Emre Akdag <emreakdag@windowslive.com>
2022-01-20 16:44:35 +0100

    duzenleme yapildi

--- src/Taksimetre.java
+++ src/Taksimetre.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Taksimetre {
+import java.util.Scanner;
+
+public class Taksimetre {
+    public static void main(String[] args) {
+        int km;
+        double perKm= 2.20, total=10;
+        Scanner input= new Scanner(System.in);
+        System.out.print("Lutfen Mesafeyi Km Cinsinden Giriniz: ");
+        km= input.nextInt();
+
+
+        total += (perKm*km);
+        total= (total<20) ? 20: total;
+        System.out.print("Odenecek Tutar : " + total);
+
+    }
 }
