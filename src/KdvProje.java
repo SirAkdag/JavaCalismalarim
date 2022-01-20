faf6ae81fc12ec17f8332462f7515717abbec5cb
Your Name <emreakdag@windowslive.com>
2022-01-20 02:04:11 +0100

    duzenlendi

--- src/KdvProje.java
+++ src/KdvProje.java
@@ -2,19 +2,21 @@ import java.util.Scanner;
 
 public class KdvProje {
     public static void main(String[] args) {
-        double tutar,kdvTutari,kdvliTutar,kdvsizTutar,kdv1=0.18,kdv2=0.08;
+        double tutar,kdv1=0.18,kdv2=0.08;
+
         Scanner input= new Scanner(System.in);
         System.out.print("Bir Tutar Giriniz :");
         tutar= input.nextInt();
 
-        boolean durum1= tutar <1000;
-        boolean durum2= tutar >1000;
+        boolean sonuc1= !(tutar<1000);
+        boolean sonuc2= tutar>=1000;
+
         double dFiyat= tutar*kdv2;
         double yFiyat= tutar*kdv1;
-        boolean girilenTutar = durum1 || durum2;
+        boolean girilenTutar = sonuc2 || sonuc1;
 
 
-        String str = girilenTutar ? "Kdv Tutari: "+dFiyat :"Kdv Tutari:"+yFiyat;
+        String str = girilenTutar ? ("Sonuc: " + dFiyat) : ("Sonuc: " + yFiyat);
         System.out.println(str);
     }
 }
