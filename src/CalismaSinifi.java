fd1f99d2887e4765c6c1106fcf954cde59f68ca5
Emre Akdag <emreakdag@windowslive.com>
2022-01-22 13:06:03 +0100

    duzenlemeler yapildi

---
+++ src/CalismaSinifi.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class CalismaSinifi {
+    public static void main(String[] args) {
+        int pati,cilli;
+        Scanner input= new Scanner(System.in);
+        System.out.print("Cilli mamasini bitirdiyse 1'e, bitirmediyse 2'e basiniz: ");
+        cilli= input.nextInt();
+    }
+}
