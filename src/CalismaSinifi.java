884378888e180950a2b642724d2cdb730c66d50e
Your Name <emreakdag@windowslive.com>
2022-01-25 00:08:24 +0100

    duzenleme

--- src/CalismaSinifi.java
+++ src/CalismaSinifi.java
@@ -2,9 +2,31 @@ import java.util.Scanner;
 
 public class CalismaSinifi {
     public static void main(String[] args) {
-        int pati,cilli;
-        Scanner input= new Scanner(System.in);
-        System.out.print("Cilli mamasini bitirdiyse 1'e, bitirmediyse 2'e basiniz: ");
-        cilli= input.nextInt();
+        int day, month;
+        String burc = "";
+        boolean isError = false;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Lütfen Dogdugunuz Ayi Giriniz :");
+        month = input.nextInt();
+        System.out.print("Lütfen Dogdugunuz Günü Giriniz: ");
+        day = input.nextInt();
+
+        if (day >= 1 && day <= 31) {
+            if (day < 22) {
+                burc = "Oglak";
+            } else {
+                burc = "Kova";
+            }
+        } else {
+            isError = true;
+        }
+        if (day >= 1 && day <= 28){
+            if (day <=19) {
+
+            }
+        }
+
+
     }
 }
