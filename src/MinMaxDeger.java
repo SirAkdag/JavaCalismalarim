3aeaf3dc2ddd4199bfd0b6b65393212a35cf853a
Your Name <emreakdag@windowslive.com>
2022-01-31 18:06:47 +0100

    duzenleme

--- src/MinMaxDeger.java
+++ src/MinMaxDeger.java
@@ -4,7 +4,7 @@ public class MinMaxDeger {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int x, numberCount = 1, tempNumber, kucuk = 0, buyuk = 0;
-        System.out.print("Kac Sayi Girmek Istiyorsunuz: ");
+        System.out.print("Kac Adet Sayi Girmek Istiyorsunuz: ");
         x = input.nextInt();
         for (int i = 1; i <= x; i++) {
             System.out.print(numberCount + ". Sayiyi Giriniz: ");
