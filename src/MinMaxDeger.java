d5207be52cccd2a7b9c80fd79b2299d6fc882fee
Your Name <emreakdag@windowslive.com>
2022-01-31 18:05:40 +0100

    yeni proje eklendi

---
+++ src/MinMaxDeger.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class MinMaxDeger {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int x, numberCount = 1, tempNumber, kucuk = 0, buyuk = 0;
+        System.out.print("Kac Sayi Girmek Istiyorsunuz: ");
+        x = input.nextInt();
+        for (int i = 1; i <= x; i++) {
+            System.out.print(numberCount + ". Sayiyi Giriniz: ");
+            tempNumber = input.nextInt();
+            numberCount++;
+
+            if (tempNumber > buyuk) {
+                buyuk = tempNumber;
+                // kucuk degiskenini 0'dan cikartip döngüye katmak icin buraya eklendi.
+                kucuk = tempNumber;
+            }else if (tempNumber <= kucuk ) {
+                kucuk = tempNumber;
+            }
+
+
+        }
+        System.out.println("\nEn Büyük: " + buyuk);
+        System.out.println("En Kücük: " + kucuk);
+
+
+    }
+}
