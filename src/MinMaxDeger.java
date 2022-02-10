321ebbb23f345de5c225f588a343dd299c168bde
Emre Akdag <emreakdag@windowslive.com>
2022-02-10 21:52:23 +0100

    duzenleme

--- src/MinMaxDeger.java
+++ src/MinMaxDeger.java
@@ -6,6 +6,7 @@ public class MinMaxDeger {
         int x, numberCount = 1, tempNumber, kucuk = 0, buyuk = 0;
         System.out.print("Kac Adet Sayi Girmek Istiyorsunuz: ");
         x = input.nextInt();
+
         for (int i = 1; i <= x; i++) {
             System.out.print(numberCount + ". Sayiyi Giriniz: ");
             tempNumber = input.nextInt();
@@ -13,13 +14,10 @@ public class MinMaxDeger {
 
             if (tempNumber > buyuk) {
                 buyuk = tempNumber;
-                // kucuk degiskenini 0'dan cikartip döngüye katmak icin buraya eklendi.
-                kucuk = tempNumber;
-            }else if (tempNumber <= kucuk ) {
+
+            }else if (tempNumber < kucuk ) {
                 kucuk = tempNumber;
             }
-
-
         }
         System.out.println("\nEn Büyük: " + buyuk);
         System.out.println("En Kücük: " + kucuk);
