48805079ba74c81555967ad17f92dab03718939b
Emre Akdag <emreakdag@windowslive.com>
2022-02-23 19:08:15 +0100

    duzenleme

--- src/ArrayLetter.java
+++ src/ArrayLetter.java
@@ -1,5 +1,26 @@
+import java.util.Arrays;
 public class ArrayLetter {
     public static void main(String[] args) {
+        String[][] matris = new String[7][4];
+        for (int i = 0; i < matris.length; i++) {
+            for (int j = 0; j < matris[i].length; j++) {
+                if (i == 0 || i == 3 || i==6){
+                    matris[i][j] = "*";
+                }else if(j == 0 || j == 3){
+                    matris[i][j] = "*";
+                }else {
+                    matris[i][j] = " ";
+                }
+
+            }
+        }
+        for (String[] row: matris){
+            System.out.println();
+            for (String col : row){
+                System.out.print(col + "  ");
+            }
+        }
+
 
     }
 }
